4e1d4b6f1323f25e316b29d5cb81baac45a6779d
Ivan Milivojevic <ivan.milivojevic@endava.com>
2019-03-02 00:50:14 +0100

    update

--- development/main.tf
+++ development/main.tf
@@ -26,4 +26,5 @@ module "vpc" {
   vpc_cidr        = "${var.vpc_cidr}"
   public_subnets  = ["${var.public_subnets}"]
   private_subnets = ["${var.private_subnets}"]
+  azs             = ["${var.azs}"]
 }
